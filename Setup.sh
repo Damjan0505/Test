397e6c04e429383cc9f03faf58ce70be286ba20d
Damjan0505 <damjan.hatic@gmail.com>
2018-11-22 22:41:55 +0100

    Corrected and Completed Sublime installation code

--- Setup.sh
+++ Setup.sh
@@ -13,8 +13,10 @@ git config --global user.name "Damjan0505"
 git config --global user.email damjan.hatic@gmail.com
 
 #Download and install Sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-y add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list^C
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 
 #Install and upgrade PIP for Python 3.6 and install all dependancies
 sudo apt-get install -y python3-pip
